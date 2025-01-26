4f72279f165e34cb5fe7a39abec736c6da2fd950
unknown <glorianamonica06@gmail.com>
2025-01-26 19:22:45 +0700

    Push Tugas2 to Repo

--- Tugas2_Monik.py
+++ Tugas2_Monik.py
@@ -13,59 +13,63 @@ def check_alphabeth (userguess):
     else:
         return False
     
-def check_answer(userguess, Secret_Word, num, usercorrectguess):
+def check_answer(userguess, Secret_Word, num):
     if userguess == Secret_Word[num].lower():
-        usercorrectguess.append(userguess)
         return True
     elif userguess == Secret_Word[num].upper():
-        usercorrectguess.append(userguess)
         return True
     else:
-        return False
-    
-def check2(userguess, usercorrectguess):
-    for i in usercorrectguess:
-        if userguess == i:
-            return True
-        else:
-            return False
+        return False 
 
-def after_check(usercorrectguess, Secret_Word):
+def after_check(usercorrectguess):
     print("correct, guessed word so far:")
     for i in usercorrectguess:
         print (i)
     print("------------------------------")
-        
-def HP(usercorrectguess, userguess):
-    Health = 6
-    for x in range (0,7):
-        if check2(userguess, usercorrectguess) is False:
-            Health -= 1
-            print ("Health:", Health)
+
+def check2(userguess, usercorrectguess, num):
+    if num >= 1:
+        if userguess.upper() in usercorrectguess:
+            return True
+        elif userguess.lower() in usercorrectguess:
+            return True
         else:
-            break
+            return False
+    else:
+        return False
 
 def Main ():
     num = 0
     usercorrectguess= []
-    print ("Health: 6")
-    while True:
+    Health = 6
+    while Health != 0:
+        print ("Health:", Health)
         userguess = input("Guess the secret word: ")
+        print("------------------------------")
         if check_alphabeth (userguess) is True:
             if check_req (userguess) is True:
-                if check_answer(userguess, Secret_Word, num, usercorrectguess) is True:
+                if check_answer(userguess, Secret_Word, num) is True:
+                    usercorrectguess.append(userguess)
                     num = num + 1
-                    after_check(usercorrectguess, Secret_Word)
-                    HP(usercorrectguess, userguess)
-                    if num == len(Secret_Word):
-                        print ("The secret word is \"Car\"")
-                        break
-                elif check_answer(userguess, Secret_Word, num, usercorrectguess) is False:
-                    print("Wrong, guess again")
+                    after_check(usercorrectguess)
+                    Health -= 1
+                elif check_answer(userguess, Secret_Word, num) is False:
+                    if check2(userguess, usercorrectguess, num) is True:
+                        print ("You already guess this letter")
+                        print("------------------------------")
+                    elif check2(userguess, usercorrectguess, num) is False:
+                        Health -= 1
+                        print("Wrong, guess again")
+                        print("------------------------------")
+                if num == len(Secret_Word):
+                    print ("The secret word is \"Car\"") 
+                    break
             elif check_req (userguess) is False:
                 print("Guess must be only a word")
         elif check_alphabeth (userguess) is False:
             print("Guess must be in alphabeth")
+    else:
+        print ("You ran out of health")
 
 
 Main()
