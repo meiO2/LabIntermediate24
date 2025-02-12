5f4a4a6ff2411a366b987212c3859c408efa517f
unknown <glorianamonica06@gmail.com>
2025-02-12 09:32:47 +0700

    Push tugas week 3 to Repo

---
+++ Tugas3_Monik.py
@@ -0,0 +1,93 @@
+Secret_Word = "car"
+
+def hasil(userguess, correctguess):
+    for x in range(len(Secret_Word)):
+        if userguess == Secret_Word[x].lower():
+            if x == 0:
+                correctguess.pop(0)
+                correctguess.insert(0, userguess)
+                return correctguess
+            elif x == 1:
+                correctguess.pop(2)
+                correctguess.insert(2, userguess)
+                return correctguess
+            elif x == 2:
+                correctguess.pop(4)
+                correctguess.insert(4, userguess)
+                return correctguess
+        elif userguess == Secret_Word[x].upper():
+            if x == 0:
+                correctguess.pop(0)
+                correctguess.insert(0, userguess)
+                return correctguess
+            elif x == 1:
+                correctguess.pop(2)
+                correctguess.insert(2, userguess)
+                return correctguess
+            elif x == 2:
+                correctguess.pop(4)
+                correctguess.insert(4, userguess)
+                return correctguess
+        elif userguess != Secret_Word[x]:
+            continue
+    return 
+
+def cek(userguess, correctguess, num):
+    if num >= 1:
+        if userguess.upper() in correctguess:
+            return True
+        elif userguess.lower() in correctguess:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def Main (Secret_Word):
+    num = 0
+    correctguess= ["_"," ","_"," ","_"]
+    Health = 6
+    while Health != 0:
+        print ("Health:", Health)
+        userguess = input("tebak kata: ")
+        print("------------------------------")
+        if userguess.isalpha():
+            if len(userguess)==1:
+                if userguess == Secret_Word[num].lower():
+                    hasil(userguess, correctguess)
+                    num = num + 1
+                    print("tebakan benar:")
+                    for i in correctguess:
+                        print (i, end="")
+                    print ("")
+                    print("------------------------------")
+                    Health -= 1
+                elif userguess == Secret_Word[num].upper():
+                    hasil(userguess, correctguess)
+                    num = num + 1
+                    for i in correctguess:
+                        print (i, end="")
+                    print("")
+                    print("------------------------------")
+                    Health -= 1
+                else:
+                    if cek(userguess, correctguess, num) is True:
+                        print ("huruf sudah ditebak, tebak lagi")
+                        print("------------------------------")
+                    elif cek(userguess, correctguess, num) is False:
+                        Health -= 1
+                        print("Salah, tebak lagi")
+                        print("------------------------------")
+                if num == len(Secret_Word):
+                    print ("Congrats, kata rahasia adalah \"Car\"") 
+                    break
+            else:
+                print("Tebakan harus 1 huruf")
+        else:
+            print("Tebakan harus huruf")
+    else:
+        print ("GAME OVER")
+        print ("Health Habis")
+
+Main(Secret_Word)
+
